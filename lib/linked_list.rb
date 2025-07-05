e3d0234a4e6a38e953d1f7baf55f1756b30e9c41
boticelli <kandelakitina@gmail.com>
2025-07-05 16:30:38 +0300

    Implement append, head and tail

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
 class LinkedList
+  attr_accessor :head, :tail
 
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(item)
+    if @head.nil?
+      @head = Node.new(item)
+      @tail = @head
+    else
+      @tail.next_node = Node.new(item)
+      @tail = @tail.next_node
+    end
+  end
+end
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
 end
