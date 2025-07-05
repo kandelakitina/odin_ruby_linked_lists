a78b93c3e38ffc95d8e114db9a1e504c9f380c3e
boticelli <kandelakitina@gmail.com>
2025-07-05 16:53:24 +0300

    Update lib

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,13 +9,35 @@ class LinkedList
   end
 
   def append(item)
-    if @head.nil?
-      @head = Node.new(item)
-      @tail = @head
-    else
-      @tail.next_node = Node.new(item)
-      @tail = @tail.next_node
+    check_head_nil(item)
+    @tail.next_node = Node.new(item)
+    @tail = @tail.next_node
+  end
+
+  def prepend(item)
+    check_head_nil(item)
+    @tail = @head
+    @head = Node.new(item)
+    @head.next_node = @tail
+  end
+
+  def size
+    counter = 0
+    current = head
+    while current.next_node
+      counter += 1
+      current = current.next_node
     end
+    counter
+  end
+
+  private
+
+  def check_head_nil(item)
+    return unless @head.nil?
+
+    @head = Node.new(item)
+    @tail = @head
   end
 end
 
